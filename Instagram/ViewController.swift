3436ae8a2bfcd64fb18278268ed14f0c32addfef
Luke Davitt <davittluke@gmail.com>
2015-03-08 11:11:36 -0400

    funcitoning login with parse

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    var signUpActive = true
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var formLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registeredLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
     
     func displayAlert(title:String, error:String){
         
@@ -23,39 +32,66 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         self.presentViewController(alert, animated: true, completion: nil)
     }
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
     
     
     @IBAction func signUp(sender: AnyObject) {
-        
         var error = ""
         
-        if username.text == "" || password.text == "" {
-            
-            error = "Please enter a username and password"
-            
-        } else {
+        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0,0,50,50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        if signUpActive == true{
             
             var user = PFUser()
             user.username = username.text
             user.password = password.text
             
-            
-            user.signUpInBackgroundWithBlock{
-                (succeeded: Bool!, signupError: NSError!) -> Void in
-                if signupError == nil{
-                    
-                } else {
-                    if let errorString = signupError.userInfo?["error"] as? NSString{
-                        error = errorString
+            if username.text == "" || password.text == "" {
+                
+                error = "Please enter a username and password"
+                
+            } else {
+                
+                user.signUpInBackgroundWithBlock{
+                    (succeeded: Bool!, signupError: NSError!) -> Void in
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    if signupError == nil{
+                        
                     } else {
-                        error = "Please try again later."
+                        if let errorString = signupError.userInfo?["error"] as? NSString{
+                            error = errorString
+                        } else {
+                            error = "Please try again later."
+                        }
+                        
+                        self.displayAlert("Could Not Sign Up", error: error)
                     }
-                    
-                    self.displayAlert("Could Not Sign Up", error: error)
+                }
+                
+            }
+        
+        }else{
+            
+            PFUser.logInWithUsernameInBackground(username.text, password:password.text) {
+                (user: PFUser!, error: NSError!) -> Void in
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if user != nil {
+                    // Yes, User Exists
+                    println("logged in")
+                } else {
+                    // No, User Doesn't Exist
+                    println("there was an error")
                 }
             }
+            
             
         }
         
@@ -64,6 +100,30 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
+    @IBAction func switchActive(sender: AnyObject) {
+        let login = ["Use the form below to Login", "Sign Up","Not Registered?", "Log In"]
+        let signUp = ["Use the form below to Sign Up", "Log In", "Already Registered?", "Sign Up"]
+        var text = []
+        println("im kinda working")
+        if signUpActive == true{
+            
+            signUpActive = false
+
+            text = login
+            
+        }else{
+            
+            signUpActive = true
+            
+            text = signUp
+        }
+        
+        formLabel.text = text[0] as? String
+        loginButton.setTitle(text[1] as? String, forState: .Normal)
+        registeredLabel.text = text[2] as? String
+        signUpButton.setTitle(text[3] as? String, forState: .Normal)
+    
+    }
     override func viewDidLoad(){
         super.viewDidLoad()
     }
